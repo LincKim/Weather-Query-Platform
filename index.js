ce82790f127232393356583da504bf1447cd8f98
LincKim <lincolnkimutai@gmail.com>
2022-12-16 12:20:55 +0300

    more js funtionality, structure & styling update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
- 
+    function removeElement() {
+        document.getElementById('div1').remove()
+    }
+    const loginButton = document.getElementById('login-click')
+    loginButton.addEventListener('click', () => {
+        removeElement()})
 })
