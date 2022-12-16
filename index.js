9f58461c2bf82fa3fa38ea18345c48ce160b5228
LincKim <lincolnkimutai@gmail.com>
2022-12-16 10:17:00 +0300

    js modifications

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function displ() {
-    let hiddenContents = document.getElementByClassName('container')
-    let hideLogin = document.getElementByClassName('login_box')
-
-        if (hiddenContents.style.display === 'none' && hideLogin.style.display === 'block') {
-        hiddenContents.style.display = 'block', hideLogin.style.display = 'none'
-        }else {
-            hiddenContents.style.display === 'none'
-        }
-}
-    document.getElementById('login-click').addEventListener('submit', () => displ())
+ 
 })
