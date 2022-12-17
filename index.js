a1fa8f1eb48be77b076b932877f37a9e5efe6ffe
LincKim <lincolnkimutai@gmail.com>
2022-12-17 20:51:35 +0300

    displaying elements on the DOM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 const cityName = 'Nairobi'
 const apiKey = '7568ec61778dabefba67c1091ef20629'
+const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 
 document.addEventListener('DOMContentLoaded', () => {
     function removeElement() {
@@ -14,7 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
     async function fetchWeather(){
         await fetch(fetchUrl)
         .then(res => res.json())
-        .then((data) => console.log(data))
+        .then((data) => showWeather(data))
         }
+    fetchWeather()
 
+    function showWeather(data){
+        const nameOfCity = data.name
+        const descript = data.weather[0].description
+        const temp = data.main.temp
+        const humid = data.main.humidity
+        const windSpeed = data.wind.speed
+        const pressure = data.main.pressure
+        console.log(nameOfCity, descript, temp, humid, windSpeed, pressure);
+
+    }
 })
