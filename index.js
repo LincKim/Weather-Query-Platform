eada6ae29501ebe068315ee1157c69e694ecdc67
LincKim <lincolnkimutai@gmail.com>
2022-12-17 22:59:26 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cityName = 'Nairobi'
+const cityName = 'nairobi'
 const apiKey = '7568ec61778dabefba67c1091ef20629'
 const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(res => res.json())
         .then((data) => showWeather(data))
         }
-    fetchWeather()
+    // fetchWeather()
 
     function showWeather(data){
         const nameOfCity = data.name
@@ -25,7 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const humid = data.main.humidity
         const windSpeed = data.wind.speed
         const pressure = data.main.pressure
-        console.log(nameOfCity, descript, temp, humid, windSpeed, pressure);
+        console.log(nameOfCity, descript, temp, humid, windSpeed, pressure)
+
+        document.getElementsByClassName('city').innerContent = nameOfCity
+        document.getElementsByClassName('temp').innerText = temp + '°C'
+        document.getElementsByClassName('description').innerText = descript
+        document.getElementsByClassName('pressure').innerText = 'Air pressure:' + pressure
+        document.getElementsByClassName('wind').innerText = 'Wind speed:' + windSpeed + 'km/h'
+        document.getElementsByClassName('humidity').innerText = 'Humidity:'+ humid + '%'
 
     }
+    function searchWorking(){
+        fetchWeather(document.getElementsByClassName('search_bar').value)
+    }
+    document.querySelector('.search_button').addEventListener('click', () => searchWorking() )
 })
