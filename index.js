669da2d6481832628caa6df1e21a7479e86d9b8c
LincKim <lincolnkimutai@gmail.com>
2022-12-17 19:47:32 +0300

    progress

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-let cityName = ''
+const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+const cityName = 'Nairobi'
 const apiKey = '7568ec61778dabefba67c1091ef20629'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,5 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('login-click')
     loginButton.addEventListener('click', () => {
         removeElement()})
+
+
+    async function fetchWeather(){
+        await fetch(fetchUrl)
+        .then(res => res.json())
+        .then((data) => console.log(data))
+        }
 
 })
