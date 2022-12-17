e264c5d7a1f93d968389a9ba8897a49af4d8f69e
LincKim <lincolnkimutai@gmail.com>
2022-12-17 16:06:33 +0300

    fetch

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fetchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+let cityName = ''
+const apiKey = '7568ec61778dabefba67c1091ef20629'
+
 document.addEventListener('DOMContentLoaded', () => {
     function removeElement() {
         document.getElementById('div1').remove()
@@ -5,5 +9,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('login-click')
     loginButton.addEventListener('click', () => {
         removeElement()})
-        
+
 })
